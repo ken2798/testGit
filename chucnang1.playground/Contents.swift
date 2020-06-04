850e8d95548d7c65ac132442aad931b51334dcd2
ken2798 <44506559+ken2798@users.noreply.github.com>
2020-06-04 13:31:20 +0700

    update chuc nang1

---
+++ chucnang1.playground/Contents.swift
@@ -0,0 +1,217 @@
+[
+    edit lan 1
+<<<<<<< HEAD
+    khong config
+=======
+
+    {
+        longitude: 21.122782, lattude:107.427210
+    },
+    {
+        longitude: 20.954437, lattude:107.100704
+    },
+    {
+        longitude: 20.959712, lattude:107.061533
+    },
+    {
+        longitude: 20.953635, lattude:107.058832
+    },
+    {
+        longitude: 20.953864, lattude:107.054043
+    },
+    {
+        longitude: 20.956092, lattude:107.049348
+    },
+    {
+        longitude: 20.948419, lattude:07.027214
+    },
+    {
+        longitude: 20.948851, lattude:107.026561
+    },
+    {
+        longitude: 20.918655, lattude:106.986899
+    },
+    {
+        longitude: 23.277513, lattude:105.250795
+    },
+    {
+        longitude: 22.821833, lattude:104.952082
+    },
+    {
+        longitude: 22.494253, lattude:103.966781
+    },
+    {
+        longitude: 22.471436, lattude:103.973516
+    },
+    {
+        longitude: 22.459680, lattude:103.975013
+    },
+    {
+        longitude: 22.274010, lattude:103.912348
+    },
+    {
+        longitude: 22.257971, lattude:103.951343
+    },
+    {
+        longitude: 22.103502, lattude:103.622050
+    },
+    {
+        longitude: 21.586397, lattude:105.648157
+    },
+    {
+        longitude: 21.376409, lattude:105.553725
+    },
+    {
+        longitude: 21.164949, lattude:105.731671
+    },
+    {
+        longitude: 21.163081, lattude:105.733388
+    },
+    {
+        longitude: 21.159789, lattude:105.733912
+    },
+    {
+        longitude: 21.154616, lattude:105.736407
+    },
+    {
+        longitude: 21.155020, lattude:105.738913
+    },
+    {
+        longitude: 21.153357, lattude:105.737178
+    },
+    {
+        longitude: 21.152368, lattude:105.740696
+    },
+    {
+        longitude: 21.152503, lattude:105.738359
+    },
+    {
+        longitude: 21.146976, lattude:105.763794
+    },
+    {
+        longitude: 21.222836, lattude:105.795734
+    },
+    {
+        longitude: 21.221357, lattude:105.799029
+    },
+    {
+        longitude: 21.218110, lattude:105.808246
+    },
+    {
+        longitude: 21.222461, lattude:105.797662
+    },
+    {
+        longitude: 21.218416, lattude:105.813636
+    },
+    {
+        longitude: 21.216026, lattude:105.816003
+    },
+    {
+        longitude: 21.216823, lattude:105.818435
+    },
+    {
+        longitude: 21.222093, lattude:105.801212
+    },
+    {
+        longitude: 21.061090, lattude:105.896992
+    },
+    {
+        longitude: 21.066929, lattude:105.882977
+    },
+    {
+        longitude: 21.044974, lattude:105.875719
+    },
+    {
+        longitude: 21.042638, lattude:105.877971
+    },
+    {
+        longitude: 21.038200, lattude:105.876970
+    },
+    {
+        longitude: 21.038900, lattude:105.866208
+    },
+    {
+        longitude: 21.059922, lattude:105.830919
+    },
+    {
+        longitude: 21.043105, lattude:105.840430
+    },
+    {
+        longitude: 21.041003, lattude:105.843683
+    },
+    {
+        longitude: 21.044740, lattude:105.842682
+    },
+    {
+        longitude: 21.045908, lattude:105.845435
+    },
+    {
+        longitude: 21.045908, lattude:105.840930
+    },
+    {
+        longitude: 21.039694, lattude:105.828764
+    },
+    {
+        longitude: 21.036396, lattude:105.813528
+    },
+    {
+        longitude: 21.034359, lattude:105.804706
+    },
+    {
+        longitude: 21.035123, lattude:105.795520
+    },
+    {
+        longitude: 21.037075, lattude:105.794975
+    },
+    {
+        longitude: 21.024075, lattude:105.808688
+    },
+    {
+        longitude: 21.026334, lattude:105.844170
+    },
+    {
+        longitude: 21.024683, lattude:105.846381
+    },
+    {
+        longitude: 21.025818, lattude:105.848261
+    },
+    {
+        longitude: 21.028089, lattude:105.843396
+    },
+    {
+        longitude: 21.029018, lattude:105.845165
+    },
+    {
+        longitude: 21.025921, lattude:105.842953
+    },
+    {
+        longitude: 21.021586, lattude:105.842843
+    },
+    {
+        longitude: 21.013018, lattude:105.831563
+    },
+    {
+        longitude: 21.012037, lattude:105.834051
+    },
+    {
+        longitude: 20.998997, lattude:105.814996
+    },
+    {
+        longitude: 20.993208, lattude:105.835665
+    },
+    {
+        longitude: 20.986695, lattude:105.848066
+    },
+    {
+        longitude: 20.991117, lattude:105.798116
+    },
+    {
+        longitude: 21.011646, lattude:105.822462
+    },
+    {
+        longitude: 20.998449, lattude:105.839477
+    },
+    {
+        longitude: 21.007980, lattude:105.851258
+    },
+]
